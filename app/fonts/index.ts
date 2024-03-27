82aa889feb48746337851e32a827d5a4db6245e5
MarcosMRod <marcos.montero@mindera.com>
2024-03-27 10:42:08 +0100

    copy

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,12 +1,14 @@
-import { Caveat, Inter } from "next/font/google";
+import { Cabin, Caveat, Inter } from "next/font/google";
 
 const caveat = Caveat({
   subsets: ["latin"],
   display: "swap",
 });
 const inter = Inter({ subsets: ["latin"] });
+const cabin = Cabin({ subsets: ["latin"] });
 
 export const fonts = {
+  cabin,
   caveat,
   inter,
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ import { CopyIcon } from "lucide-react";
 import Link from "next/link";
 import { inViewSensor } from "tailwind-inview";
 
+import { cn } from "@/lib/utils";
+
 import { Arrow } from "./components/arrow";
 import { Coin } from "./components/coin";
 import { GithubLogo } from "./components/github";
+import { fonts } from "./fonts";
 
 export default function Home() {
   useEffect(inViewSensor, []);
@@ -23,7 +26,14 @@ export default function Home() {
             key={n}
             className="container h-screen snap-center flex flex-wrap items-center gap-16 justify-center flex-col"
           >
-            <p className="text-white text-[80px]">As simple as:</p>
+            <p
+              className={cn(
+                "text-white text-[80px] p-0 m-0 text-center",
+                fonts.cabin.className
+              )}
+            >
+              As simple as:
+            </p>
             <div
               className={
                 "shadow-2xl rounded-xl bg-white/40 h-0 overflow-hidden border-y-white duration-1000 px-4 flex items-center justify-between inview:animate-shake inview:border-y-[16px] blur-sm inview:blur-none inview:h-24"
@@ -35,9 +45,8 @@ export default function Home() {
               </code>
             </div>
             <button onClick={copyCommand} className="group">
-              <h3 className="italic text-white text-center text-2xl bg-black/80 rounded-lg px-4 py-2 flex gap-2 items-center">
-                npm i{" "}
-                <span className="font-bold text-2xl">tailwind-inview</span>{" "}
+              <h3 className=" text-white/80 text-center text-2xl bg-black/80 rounded-lg px-4 py-2 flex gap-2 items-center">
+                npm i<span className=" text-white">tailwind-inview</span>{" "}
                 <CopyIcon className="text-white/40 group-hover:text-white/90  duration-300 text-sm ml-4" />
               </h3>
             </button>
